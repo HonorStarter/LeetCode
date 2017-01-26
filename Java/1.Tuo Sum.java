75730750c9f4be7e6222d01d680b41ad20fd2634
HonorStarter <zonghyhit@163.com>
2017-01-27 01:40:54 +0800

    Update 1.Tuo Sum.java

--- Java/1.Tuo Sum.java
+++ Java/1.Tuo Sum.java
@@ -1,3 +1,10 @@
+//2017年春节做的第一道题，一切重新开始！
+/*题目：求数组中两数之和等于target的，返回数组
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].*/
+
+//我的答案：
 public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for(int i=0;i<nums.length;i++)
